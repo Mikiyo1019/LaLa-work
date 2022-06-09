8a787e066ee0ef7bd4d696dfdcddb289fa87c4f8
Mikiyo1019 <mikiyogone1019@gmail.com>
2022-06-09 16:29:28 +0900

    plus

--- sukkiri/src/chap15/Main585.java
+++ sukkiri/src/chap15/Main585.java
@@ -9,13 +9,16 @@ public class Main585 {
 		SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 		
 		//文字列からDateインスタンスを生成
-		Date d = f.parse("2020/09/22 01:23");
+		Date d = f.parse("2025/09/22 01:23");
 		System.out.println(d);
 		
 		//Dateインスタンスから文字列を生成
 		Date now = new Date();
-		String s = f.format(now);
-		System.out.println("現在は" + s + "です");
+		int day = (int)(d.getTime() - now.getTime()) / (24*60*60*1000) ;
+		
+		//String s = f.format(now);
+		//System.out.println("現在は" + s + "です");
+		System.out.println(day);
 	}
 
 }
